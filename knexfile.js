fd2eb51b1b013ee676985df939e340e9ca531f49
Casey Foster <c@sey.me>
2015-03-21 08:29:31 -0500

    Fix migration

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 var config = require('./build/node_modules/shared/config');
 
 var knexConfig = {};
-knexConfig[config.env] = config.knex;
+knexConfig[process.env.NODE_ENV || 'development'] = config.knex;
 
 module.exports = knexConfig;
