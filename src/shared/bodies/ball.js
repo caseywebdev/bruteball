a527daadfca4b33f9a1a5cbb9075d524715bea19
Casey Foster <c@sey.me>
2017-04-06 19:42:08 -0500

    Update dependencies

--- src/shared/bodies/ball.js
+++ src/shared/bodies/ball.js
@@ -1,23 +1,24 @@
-import b2 from 'box2d.js';
+import {CircleShape, Vec2} from 'planck-js';
 import config from '../config';
 
-const BODY_DEF = new b2.b2BodyDef();
-BODY_DEF.set_type(b2.b2_dynamicBody);
-BODY_DEF.set_fixedRotation(true);
-BODY_DEF.set_linearDamping(config.game.linearDamping);
+const {ballRadius, linearDamping} = config.game;
 
-const SHAPE = new b2.b2CircleShape();
-SHAPE.set_m_radius(config.game.ballRadius);
+const BODY_DEF = {
+  type: 'dynamic',
+  fixedRotation: true,
+  linearDamping
+};
 
-const FIXTURE_DEF = new b2.b2FixtureDef();
-FIXTURE_DEF.set_shape(SHAPE);
-FIXTURE_DEF.set_density(1);
-FIXTURE_DEF.set_restitution(0.2);
-FIXTURE_DEF.set_friction(0);
+const FIXTURE_DEF = {
+  shape: new CircleShape(ballRadius),
+  density: 1,
+  restitution: 0.2,
+  friction: 0
+};
 
 export default ({game, x, y}) => {
-  BODY_DEF.get_position().Set(x, y);
-  var body = game.world.CreateBody(BODY_DEF);
-  body.CreateFixture(FIXTURE_DEF);
+  const body = game.world.createBody(BODY_DEF);
+  body.setPosition(new Vec2(x, y));
+  body.createFixture(FIXTURE_DEF);
   return body;
 };
