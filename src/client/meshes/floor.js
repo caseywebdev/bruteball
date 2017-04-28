8323380867a8057c351f6e4823316608b9f9ea31
Casey Foster <c@sey.me>
2017-04-28 10:21:38 -0500

    Add dirt floor

--- src/client/meshes/floor.js
+++ src/client/meshes/floor.js
@@ -1,14 +1,20 @@
 import {
   Mesh,
   MeshLambertMaterial,
-  PlaneBufferGeometry
+  PlaneBufferGeometry,
+  RepeatWrapping,
+  TextureLoader
 } from 'three';
 
 const MAP_SIZE = 32;
 
 const GEOMETRY = new PlaneBufferGeometry(MAP_SIZE, MAP_SIZE);
 
-const MATERIAL = new MeshLambertMaterial();
+const TEXTURE = (new TextureLoader()).load('/textures/dirt.jpg');
+TEXTURE.wrapS = TEXTURE.wrapT = RepeatWrapping;
+TEXTURE.repeat.set(5, 5);
+
+const MATERIAL = new MeshLambertMaterial({map: TEXTURE});
 
 export default class extends Mesh {
   constructor() {
